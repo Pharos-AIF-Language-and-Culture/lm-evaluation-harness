b0f571121a65f7a91dce059afde2e30d2d5f3a7f
Leon_V <leonvouk@gmail.com>
2025-12-22 20:52:51 +0200

    ifeval_el fixes

--- lm_eval/tasks/pharos/ifeval_el/instructions_registry.py
+++ lm_eval/tasks/pharos/ifeval_el/instructions_registry.py
@@ -14,7 +14,7 @@
 
 """Registry of all instructions."""
 
-from lm_eval.tasks.ifeval import instructions
+from lm_eval.tasks.pharos.ifeval_el import instructions
 
 
 _KEYWORD = "keywords:"
--- lm_eval/tasks/pharos/ifeval_el/utils.py
+++ lm_eval/tasks/pharos/ifeval_el/utils.py
@@ -1,7 +1,7 @@
 import dataclasses
 from typing import Dict, Optional, Union
 
-from lm_eval.tasks.ifeval import instructions_registry
+from lm_eval.tasks.pharos.ifeval_el import instructions_registry
 
 
 @dataclasses.dataclass
